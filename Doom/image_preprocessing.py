db5dbe2696a98782b8b266f95a2f321a19098519
Kaustabh Ganguly <stabgan@users.noreply.github.com>
2026-03-14 16:22:07 +0530

    Revert "fix: modernize code + rewrite README"
    This reverts commit 831e3ca5080ab6f49fe2086610e90efb66f429b4.

--- Doom/image_preprocessing.py
+++ Doom/image_preprocessing.py
@@ -2,27 +2,27 @@
 
 # Importing the libraries
 import numpy as np
-from PIL import Image
-import gymnasium as gym
-from gymnasium.spaces import Box
+from scipy.misc import imresize
+from gym.core import ObservationWrapper
+from gym.spaces.box import Box
 
 # Preprocessing the Images
 
-class PreprocessImage(gym.ObservationWrapper):
-
-    def __init__(self, env, height=64, width=64, grayscale=True, crop=lambda img: img):
+class PreprocessImage(ObservationWrapper):
+    
+    def __init__(self, env, height = 64, width = 64, grayscale = True, crop = lambda img: img):
         super(PreprocessImage, self).__init__(env)
         self.img_size = (height, width)
         self.grayscale = grayscale
         self.crop = crop
         n_colors = 1 if self.grayscale else 3
-        self.observation_space = Box(0.0, 1.0, shape=(n_colors, height, width), dtype=np.float32)
+        self.observation_space = Box(0.0, 1.0, [n_colors, height, width])
 
-    def observation(self, img):
+    def _observation(self, img):
         img = self.crop(img)
-        img = np.array(Image.fromarray(img).resize((self.img_size[1], self.img_size[0])))
+        img = imresize(img, self.img_size)
         if self.grayscale:
-            img = img.mean(-1, keepdims=True)
+            img = img.mean(-1, keepdims = True)
         img = np.transpose(img, (2, 0, 1))
         img = img.astype('float32') / 255.
         return img
